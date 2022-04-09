3e08df2d81638a9f509abe0ed71a540048513b4d
Luke Diebold <luke@ldiebold.com>
2022-04-09 23:39:28 +1000

    remove config

--- server/index.js
+++ server/index.js
@@ -1,13 +1,11 @@
 'use strict';
 
-const config = require('./config');
 const controllers = require('./controllers');
 const routes = require('./routes');
 const middlewares = require('./middlewares');
 const services = require('./services');
 
 module.exports = {
-  config,
   controllers,
   routes,
   services,
